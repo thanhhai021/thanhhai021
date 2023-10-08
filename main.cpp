01d3b515187c7ae907389a5a25918825fa0daa82
thanhhai021 <147145382+thanhhai021@users.noreply.github.com>
2023-10-08 10:11:44 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int tuoi; //khai bao kieu du lieu bien
+    float luonghangngay;
+    float luonghangThang;
+    float Luonghangtuan;
+
+    float luonghangngay=350000; // gan gia tri
+    float luonghangThang=10Tr;
+    float Luonghangtuan=2Tr5;
+
+    int a,b,c=5; //khai bao nhieu bien cung 1 lenh
+
+    char chon = '9'; //gan ky tu
+
+    bool X =true; // khai bao dung sai
+
+    typedef int INT; // tao alias cho bien (int, double,...)
+
+    const float PI = 3.14; // khai bao hang
+    const int Size =39;
+    #define PI 3.14  // khai bao hang
+    #define SIZE 40
+
+    return 0;
+}
+
